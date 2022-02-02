7ce64f9bb10446112c4ecbe3b76f5aaf12f47428
Michejs <ielitromichele@gmail.com>
2022-02-02 16:22:20 +0100

    esercizio 3 completed

---
+++ angular-ex/src/app/interface/users-interface.ts
@@ -0,0 +1,28 @@
+export interface User {
+    id: number,
+    name: string,
+    surname: string,
+    age: number,
+    dateOfBirth: string,
+    address: iAddress,
+    role: Role,
+    username: string,
+    profilePhotoUrl: string,
+    gender: Gender
+}
+
+export enum Gender {
+    MALE,
+    FEMALE,
+    OTHER,
+}
+export enum Role {
+    STAFF,
+    MANAGER,
+    ADMIN,
+}
+interface iAddress {
+    city: string;
+    street: string;
+    postalCode: string;
+}
